4d5a820e3c8c2d199ad1f78206e63105af99bc78
Arnaud De Coster <arnaud@getready.io>
2018-07-13 09:06:12 -0400

    Initial commit : gitignore, behaviors, manager, settings ...

---
+++ Assets/Scripts/AbcBuilderExportOnly.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbcBuilderExportSpecific : MonoBehaviour {
+
+    private MyManager myManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myManager.BuilderExportOnlyMethod();    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
